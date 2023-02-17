a728359e9cf98c34e98acfc2897c00d3fa3cb4b2
g.lim <lim215@purdue.edu>
2023-02-16 20:26:21 -0500

    update parameter and branch 4

--- density_planner/hyperparams.py
+++ density_planner/hyperparams.py
@@ -126,9 +126,9 @@ def parse_args():
 
     # optimization with gradient descent
     parser.add_argument('--mp_optimizer', type=str, default="Adam")
-    parser.add_argument('--mp_epochs', type=int, default=20) #100
-    parser.add_argument('--mp_epochs_density', type=int, default=50) #100
-    parser.add_argument('--mp_numtraj', type=float, default=20) #30
+    parser.add_argument('--mp_epochs', type=int, default=100) #100
+    parser.add_argument('--mp_epochs_density', type=int, default=100) #100
+    parser.add_argument('--mp_numtraj', type=float, default=30) #30
     parser.add_argument('--mp_lr', type=float, default=1e-2)
     parser.add_argument('--mp_lr_step', type=int, default=0)
     parser.add_argument('--max_gradient', type=float, default=0.02)
